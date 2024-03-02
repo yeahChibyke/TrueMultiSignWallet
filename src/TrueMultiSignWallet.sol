3c7dd4c6ce3c167e1794af6748976e4ed73a5cb1
yeahChibyke <chibykepride@gmail.com>
2024-03-02 03:54:13 +0100

    second commit

--- src/TrueMultiSignWallet.sol
+++ src/TrueMultiSignWallet.sol
@@ -14,30 +14,73 @@ contract TrueMultiSignWallet {
     event RevokeConfirmation(address indexed trueOwner, uint indexed txIndex);
     event ExecuteTransaction(address indexed trueOwner, uint indexed txIndex);
 
+    // array storing the addresses of the true owners
     address[] public trueOwners;
+    //  mapping to quickly check if an address is a true owner of the wallet.
     mapping(address => bool) public isTrueOwner;
-    uint public numConfirmationsRequired;
+    // number of confirmations required to execute a transaction
+    uint256 public numConfirmationsRequired;
 
+    // structure of a transaction
     struct Transaction {
         address to;
-        uint value;
+        uint256 value;
         bytes data;
         bool executed;
-        uint numConfirmations;
+        uint256 numConfirmations;
     }
 
     // mapping from tx index => trueOwner => bool
-    mapping(uint => mapping(address => bool)) public isConfirmed;
+    mapping(uint256 => mapping(address => bool))
+        public isConfirmed; /*keeps track of which transactions have been confirmed by which true owners*/
 
+    // array to store all transactions proposed in the wallet
     Transaction[] public transactions;
 
+    // ensures a transaction can be called by only a true owner of the wallet
     modifier onlyTrueOwner() {
         require(isTrueOwner[msg.sender], "not true owner!");
         _;
     }
 
-    modifier txExists(uint _txIndex) {
+    // checks a transaction exists
+    modifier txExists(uint256 _txIndex) {
         require(_txIndex < transactions.length, "tx does not exist");
         _;
+    }
+
+    // ensures a transaction has not been executed
+    modifier notExecuted(uint256 _txIndex) {
+        require(!transactions[_txIndex].executed, "tx already executed");
+        _;
+    }
+
+    // checks a transaction has not been confirmed by msg.sender
+    modifier notConfirmed(uint256 _txIndex) {
+        require(!isConfirmed[_txIndex][msg.sender], "tx already confirmed");
+        _;
+    }
+
+    constructor(
+        address[] memory _trueOwners,
+        uint256 _numConfirmationsRequired
+    ) {
+        require(_trueOwners.length > 0, "true owners required!");
+        require(
+            _numConfirmationsRequired > 0 &&
+                _numConfirmationsRequired <= _trueOwners.length,
+            "invalid number of required confirmations"
+        );
+
+        for (uint256 i = 0; i < _trueOwners.length; i++) {
+            address trueOwner = _trueOwners[i];
+
+            require(trueOwner != address(0), "invalid owner!");
+            require(!isTrueOwner[trueOwner], "owner not unique!");
+
+            isTrueOwner[trueOwner] = true;
+            trueOwners.push(trueOwner);
+        }
+        numConfirmationsRequired = _numConfirmationsRequired;
     }
 }
